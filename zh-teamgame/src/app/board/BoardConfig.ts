ec3b37725ea6009b24afffca3c5cef88ab316df3
zero <squiredd@gmail.com>
2022-11-25 14:18:50 -0800

    add board config

--- zh-teamgame/src/app/board/BoardConfig.ts
+++ zh-teamgame/src/app/board/BoardConfig.ts
@@ -1,8 +1,8 @@
 import { Observable } from "rxjs";
 
 export interface BoardConfig {
-    rowCount?: number | Observable<number>;
-    rowSize?: number | Observable<number>;
-    columnCount?: number | Observable<number>;
-    columnSize?: number | Observable<number>;
+    rowCount?: number;
+    rowSize?: number;
+    columnCount?: number;
+    columnSize?: number;
 }
--- zh-teamgame/src/app/board/board.component.ts
+++ zh-teamgame/src/app/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { isObservable, Observable, Subscription } from 'rxjs';
 import { BoardConfig } from './BoardConfig';
 
 @Component({
@@ -7,23 +8,61 @@ import { BoardConfig } from './BoardConfig';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+  private _boardConfig = BoardComponent.DefaultBoardConfig;
+  private _configSubscription: Subscription | undefined;
   @Input()
-  public boardConfig: BoardConfig = BoardComponent.DefaultBoardConfig;
-  private static DefaultBoardConfig: BoardConfig = {
+  set boardConfig(val: BoardConfig | Observable<BoardConfig>) {
+    if (this._configSubscription) {
+      this._configSubscription.unsubscribe();
+    }
+
+    if (isObservable(val)) {
+      this._configSubscription = val.subscribe(c => {
+        this._boardConfig = this.sanitizeConfig(c);
+        this.setConfig(this._boardConfig);
+      });
+      return;
+    }
+    this._boardConfig = this.sanitizeConfig(val);
+    this.setConfig(this._boardConfig);
+  }
+  public constructor() {
+    this.setConfig(this._boardConfig);
+  }
+  private sanitizeConfig(config: BoardConfig): SanitizedBoardConfig {
+    const sanitized = Object.assign({}, BoardComponent.DefaultBoardConfig, config);
+    if (!sanitized.columnCount || sanitized.columnCount < 1) {
+      sanitized.columnCount = BoardComponent.DefaultBoardConfig.columnCount;
+    }
+
+    if (!sanitized.rowCount || sanitized.rowCount < 1) {
+      sanitized.rowCount = BoardComponent.DefaultBoardConfig.rowCount;
+    }
+
+    if (!sanitized.columnSize || sanitized.columnSize < 1) {
+      sanitized.columnSize = BoardComponent.DefaultBoardConfig.columnSize;
+    }
+
+    if (!sanitized.rowSize || sanitized.rowSize < 1) {
+      sanitized.rowSize = BoardComponent.DefaultBoardConfig.rowSize;
+    }
+    return sanitized;
+  }
+  private setConfig(config: SanitizedBoardConfig): void {
+    this._columnDefinitions = this.getGridDefinition(config.columnSize, config.columnCount);
+    this._rowDefinitions = this.getGridDefinition(config.rowSize, config.rowCount);
+  }
+  private static readonly DefaultBoardConfig: SanitizedBoardConfig = {
     columnCount: 5,
     columnSize: 100,
     rowCount: 5,
     rowSize: 100
   }
-  private _columnWidth = 100;
-  private _columnCount = 5;
-  private _columnDefinitions: string = this.getGridDefinition(this._columnWidth, this._columnCount);
+  private _columnDefinitions = "";
   get columnDefinitions(): string {
     return this._columnDefinitions;
   }
-  private _rowWidth = 100;
-  private _rowCount = 5;
-  private _rowDefinitions: string = this.getGridDefinition(this._rowWidth, this._rowCount);
+  private _rowDefinitions = "";
   get rowDefinitions(): string {
     return this._rowDefinitions;
   }
@@ -34,6 +73,13 @@ export class BoardComponent {
       .map(_ => pixelSize)
       .reduce((prev, curr) => `${prev} ${curr}`);
   }
+}
+
+interface SanitizedBoardConfig {
+  rowCount: number;
+  rowSize: number;
+  columnCount: number;
+  columnSize: number;
 }
 
 
