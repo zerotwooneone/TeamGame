0487d75c044c25e9946be57a57d55a0d2e8fd466
zero <squiredd@gmail.com>
2022-11-25 14:22:33 -0800

    add comments

--- zh-teamgame/src/app/board/BoardConfig.ts
+++ zh-teamgame/src/app/board/BoardConfig.ts
@@ -1,8 +1,12 @@
 import { Observable } from "rxjs";
 
 export interface BoardConfig {
+    /**number of rows on board */
     rowCount?: number;
+    /**height in pixels of each row*/
     rowSize?: number;
+    /**number of columns on board */
     columnCount?: number;
+    /**width in pixels of each column*/
     columnSize?: number;
 }
